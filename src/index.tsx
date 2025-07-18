a622b6769db79dcb07c3a485ac98f3f4d1a247da
Ivan <linnorm.ya.ru>
2025-07-18 22:27:24 +0300

    Basic styles, fonts, icons, images added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { App } from './app';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('app') as HTMLElement);
 
