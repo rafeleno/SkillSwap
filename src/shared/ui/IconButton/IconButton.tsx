95db022cb5a027b1e496da28dc35504bf2071ef2
tantsyuradanil <tantsyuradanil@gmail.com>
2025-07-19 22:13:28 +0300

    test import

--- src/shared/ui/IconButton/IconButton.tsx
+++ src/shared/ui/IconButton/IconButton.tsx
@@ -2,21 +2,12 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { IconbuttonProps } from './IconButton.types';
 
-// Пример нерабочего импорта с алиасом
-// import svg from '@svg/main/add.svg';
-
-// Пример нерабочего импорта без алиаса
-// import svg from '../../../../public/svg/main/add.svg';
 
 export const IconButton: React.FC<IconbuttonProps> = ({ name }) => {
   return (
     <button>
       <svg>
-        {/* Попытка подключения с алиасом */}
-        <use xlinkHref={`@svg/main/${name}.svg`} />
-
-        {/* Пример попытки подключения без алиаса */}
-        {/*<use xlinkHref={`../../../../public/svg/main/${name}.svg`} /> */}
+        <use xlinkHref={`/svg/main/${name}.svg`} />
       </svg>
     </button>
   );
