8b67a7d461636f6d7182c05918fc1ebe228e2aa8
tantsyuradanil <tantsyuradanil@gmail.com>
2025-07-21 20:16:12 +0300

    add support of cirillic paths

--- spriteCreator/plugin.mjs
+++ spriteCreator/plugin.mjs
@@ -29,8 +29,10 @@ export default class SvgSpritePlugin {
     }
 
     generateSprite() {
-        const scriptPath = new URL(__dirname + '/generator.mjs', import.meta.url).pathname;
-        const child = spawn('node', [scriptPath], {
+        // Используем fileURLToPath для корректного преобразования пути
+        const scriptPath = new URL('./generator.mjs', import.meta.url);
+
+        const child = spawn('node', [fileURLToPath(scriptPath)], {
             stdio: 'inherit',
             env: { ...process.env, SVG_SPRITE_ONCE: 'true' }
         });
