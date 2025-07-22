37c486c74f45f5a598fecd6a90d8dd2b35f2b2a1
Valeriia Shportko <v.shportko12@gmail.com>
2025-07-22 15:32:57 +0200

    feat: добавлена верстка LikeButton

--- src/shared/ui/LikeButton/LikeButton.tsx
+++ src/shared/ui/LikeButton/LikeButton.tsx
@@ -2,7 +2,11 @@ import type { LikeButtonProps } from './LikeButton.types'
 import React from 'react'
 import styles from './styles.module.scss'
 
-export const LikeButton: React.FC<LikeButtonProps> = ({ onClick, isActive = false, className = '' }) => {
+export const LikeButton: React.FC<LikeButtonProps> = ({
+  onClick,
+  isActive = false,
+  className = '',
+}) => {
   return (
     <button
       type="button"
@@ -10,8 +14,8 @@ export const LikeButton: React.FC<LikeButtonProps> = ({ onClick, isActive = fals
       className={`${styles.button} ${className}`}
       aria-label="Поставить лайк"
     >
-      <svg className={`${styles.icon} ${isActive ? styles.active : ''}`}>
-        <use href={`/sprites.svg#${isActive ? 'like-active' : 'like'}`} />
+      <svg className={`${styles.icon} ${isActive ? styles.active : ''}`} aria-hidden="true">
+        <use href="/sprites.svg#like" />
       </svg>
     </button>
   )
