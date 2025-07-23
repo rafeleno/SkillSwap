1e21a8f2cf1afce73e0f25fac89d9fdd306f153c
Ivan <linnorm@ya.ru>
2025-07-23 16:11:53 +0300

    refactor:change svg icon to scss icon

--- src/shared/ui/MainLogo/MainLogo.tsx
+++ src/shared/ui/MainLogo/MainLogo.tsx
@@ -1,16 +1,15 @@
 import type { MainLogoProps } from './MainLogo.types'
 import React from 'react'
 import styles from './styles.module.scss'
+import { Link } from 'react-router-dom'
 
 export const MainLogo: React.FC<MainLogoProps> = () => {
   return (
-    <div className={styles.mainLogo}>
-      <div className={styles.mainLogoContainer}>
-        <svg className={styles.icon}>
-          <use xlinkHref="/sprites.svg#logo-star" />
-        </svg>
+    <Link className={styles.mainLogo} to='/'>
+      <div className={styles.iconWrapper}>
+        <span className={styles.icon}></span>
       </div>
       <h2>SkillSwap</h2>
-    </div>
+    </Link>
   )
 }
