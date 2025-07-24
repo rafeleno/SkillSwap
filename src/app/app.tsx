8ec28aea3c66ea837b5c6d0049c11fdde48813d1
Katherine Vasilevskaya <katevasilevskaya0@gmail.com>
2025-07-24 17:11:53 +0300

    fix: resolve merge conflict in app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,16 @@
+import { Datepicker } from '@uiComponents/Datepicker'
 import React from 'react'
 
 export function App() {
   return (
-    <div>
+    <>
+      <div>
 
-    </div>
+      </div>
+
+      <div style={{ padding: '2rem' }}>
+        <Datepicker />
+      </div>
+    </>
   )
 }
