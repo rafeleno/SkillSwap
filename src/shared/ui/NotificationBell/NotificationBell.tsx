aa63d7ec98e4ed29499a247f13fd4013605428f3
Valeriia Shportko <v.shportko12@gmail.com>
2025-07-25 13:44:14 +0200

    feat: добавлен компонент NotificationBell

---
+++ src/shared/ui/NotificationBell/NotificationBell.tsx
@@ -0,0 +1,18 @@
+import type { NotificationBellProps } from './NotificationBell.types'
+import React from 'react'
+import styles from './styles.module.scss'
+
+export const NotificationBell: React.FC<NotificationBellProps> = ({ onClick, isActive = false }) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`${styles.button} ${isActive ? styles.active : ''}`}
+      aria-label="Уведомления"
+    >
+      <svg className={styles.icon} aria-hidden="true">
+        <use href="/sprites.svg#notification" />
+      </svg>
+    </button>
+  )
+}
