928f4e35ef6ff83eb69529d64f1a8324cccf45e4
KusaTea <matthewkhoimov@yandex.ru>
2025-07-25 14:35:08 +0200

    feat: добавлена верстка компонента Notification

---
+++ src/shared/ui/Notification/Notification.tsx
@@ -0,0 +1,42 @@
+import type { NotificationProps } from './Notification.types'
+import { MainButton } from '@uiComponents/MainButton'
+import React from 'react'
+import styles from './styles.module.scss'
+
+export const Notification: React.FC<NotificationProps> = ({
+  isNew,
+  notificationTitle,
+  notificationText,
+  notificationDate,
+  onClick,
+}) => {
+  return (
+    <>
+      <div className={styles.notificationContainer}>
+        <div className={styles.notificationContent}>
+          <div className={styles.notificationIconAndText}>
+            <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <use href="/sprites.svg#idea" />
+            </svg>
+            <span>
+              <p className={styles.notificationTitle}>{notificationTitle}</p>
+              <p className={styles.notificationText}>{notificationText}</p>
+            </span>
+          </div>
+          <span className={styles.notificationDate}>{notificationDate}</span>
+        </div>
+        {isNew && (
+          <>
+            <span className={styles.notificationButton}>
+              <MainButton
+                type="primary"
+                children="Перейти"
+                onClick={onClick}
+              />
+            </span>
+          </>
+        )}
+      </div>
+    </>
+  )
+}
---
+++ src/shared/ui/Notification/Notification.types.ts
@@ -0,0 +1,7 @@
+export interface NotificationProps {
+  isNew: boolean
+  notificationTitle: string
+  notificationText: string
+  notificationDate: string
+  onClick: () => void
+}
