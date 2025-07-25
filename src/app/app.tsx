8102fc8391fc20fdc9bcad705a1615d2413b29fb
Victor Baskakov @rafeleno/viktorbaskakov356@gmail.com <viktorbaskakov356@gmail.com>
2025-07-25 20:02:10 +0300

    fix: clear app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,9 @@
-import { SkillCard } from '@widgetComponents/SkillCard'
 import React from 'react'
 
 export function App() {
   return (
     <>
-      <div>
-        <SkillCard type="view"></SkillCard>
-      </div>
+
     </>
   )
 }
