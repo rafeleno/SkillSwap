5ba6e3a89915c26305ff16813990598ea001427a
KusaTea <matthewkhoimov@yandex.ru>
2025-07-26 17:46:11 +0200

    feat: добавлена доступность

--- src/shared/ui/Notification/Notification.tsx
+++ src/shared/ui/Notification/Notification.tsx
@@ -12,29 +12,44 @@ export const Notification: React.FC<NotificationProps> = ({
 }) => {
   return (
     <>
-      <div className={styles.notificationContainer}>
-        <div className={styles.notificationContent}>
+      <div
+        className={styles.notificationContainer}
+        aria-label="Уведомление"
+      >
+        <div
+          className={styles.notificationContent}
+          aria-hidden="true"
+        >
           <div className={styles.notificationIconAndText}>
-            <svg width="40" height="40" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="40"
+              height="40"
+              viewBox="0 0 36 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              focusable="false"
+            >
               <use href="/sprites.svg#idea" />
             </svg>
             <span>
-              <p className={styles.notificationTitle}>{notificationTitle}</p>
+              <h4 className={styles.notificationTitle}>{notificationTitle}</h4>
               <p className={styles.notificationText}>{notificationText}</p>
             </span>
           </div>
           <span className={styles.notificationDate}>{notificationDate}</span>
         </div>
         {isNew && (
-          <>
-            <span className={styles.notificationButton}>
-              <MainButton
-                type="primary"
-                children={<p className={styles.notificationButtonText}>Перейти</p>}
-                onClick={onClick}
-              />
-            </span>
-          </>
+          // span необходим, чтобы ограничить размеры кнопки - она занимает 100% ширины родителя
+          <span className={styles.notificationButton}>
+            <MainButton
+              type="primary"
+              onClick={onClick}
+              aria-label={`Перейти к уведомлению: ${notificationTitle}`}
+            >
+              <p className={styles.notificationButtonText}>Перейти</p>
+            </MainButton>
+          </span>
         )}
       </div>
     </>
