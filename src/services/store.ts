b1730a4150b9210b2926efa339ce5214023d3f00
Katherine Vasilevskaya <katevasilevskaya0@gmail.com>
2025-07-26 14:20:07 +0300

    feat: add notifications slice

---
+++ src/services/store.ts
@@ -0,0 +1,25 @@
+import type {
+  TypedUseSelectorHook,
+} from 'react-redux'
+
+import { combineSlices, configureStore } from '@reduxjs/toolkit'
+import {
+  useDispatch as dispatchHook,
+  useSelector as selectorHook,
+} from 'react-redux'
+import { notificationsSlice } from './slices/notifications/notificationsSlice'
+
+const rootReducer = combineSlices(notificationsSlice)
+
+const store = configureStore({
+  reducer: rootReducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export type AppDispatch = typeof store.dispatch
+
+export const useDispatch: () => AppDispatch = dispatchHook
+export const useSelector: TypedUseSelectorHook<RootState> = selectorHook
+
+export default store
