3fbaecd3cee61ff279a0c73ecacde6fe53cedfa5
KusaTea <matthewkhoimov@yandex.ru>
2025-07-26 18:12:58 +0200

    feat: доабвлена кнопка и onClose для крестика, добавлена доступность

--- src/shared/ui/ToastNotification/ToastNotification.tsx
+++ src/shared/ui/ToastNotification/ToastNotification.tsx
@@ -6,33 +6,45 @@ export const ToastNotification: React.FC<ToastNotificationProps> = ({
   toastNotificationText,
   isHovered,
   onClick,
+  onClose,
 }) => {
   return (
-    <>
-      <div className={styles.toastNotificationContainer}>
-        <span className={styles.toastNotificationCross}>
-          <svg width="24" height="24" viewBox="0 0 11 10" fill="#253017" xmlns="http://www.w3.org/2000/svg">
-            <use href="/sprites.svg#cross" />
-          </svg>
-        </span>
-        <div className={styles.toastNotificationContent}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <use href="/sprites.svg#idea" />
-          </svg>
-          <p className={styles.toastNotificationText}>{toastNotificationText}</p>
-        </div>
-        {isHovered
-          && (
-            <>
-              <button
-                className={styles.toastNotificationButton}
-                onClick={onClick}
-              >
-                Перейти
-              </button>
-            </>
-          )}
+    <div className={styles.toastNotificationContainer}>
+      <button
+        className={styles.toastNotificationCross}
+        type="button"
+        onClick={onClose}
+        aria-label="Закрыть всплывающие уведомление"
+      >
+        <svg width="10" height="10" viewBox="7 7 10 10" fill="#253017" xmlns="http://www.w3.org/2000/svg">
+          <use href="/sprites.svg#cross" transform="scale(2.4)" />
+        </svg>
+      </button>
+      <div className={styles.toastNotificationContent}>
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+        >
+          <use href="/sprites.svg#idea" />
+        </svg>
+        <p className={styles.toastNotificationText}>{toastNotificationText}</p>
       </div>
-    </>
+      {isHovered
+        && (
+          <button
+            type="button"
+            className={styles.toastNotificationButton}
+            onClick={onClick}
+            aria-label={`Перейти к уведомлению: ${toastNotificationText}`}
+          >
+            Перейти
+          </button>
+        )}
+    </div>
   )
 }
--- src/shared/ui/ToastNotification/ToastNotification.types.ts
+++ src/shared/ui/ToastNotification/ToastNotification.types.ts
@@ -2,4 +2,5 @@ export interface ToastNotificationProps {
   toastNotificationText: string
   isHovered: boolean
   onClick: () => void
+  onClose: (e: React.MouseEvent) => void
 }
