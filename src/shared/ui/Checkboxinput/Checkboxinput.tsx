bc78aa258bb1813be886fd6ed4a6a211c2d6b7f1
Victor Baskakov @rafeleno/viktorbaskakov356@gmail.com <viktorbaskakov356@gmail.com>
2025-07-30 01:34:09 +0300

    remove: legacy from CheckboxInput.tsx

--- src/shared/ui/Checkboxinput/Checkboxinput.tsx
+++ src/shared/ui/Checkboxinput/Checkboxinput.tsx
@@ -3,92 +3,6 @@ import { useDispatch, useSelector } from 'react-redux'
 import { selectFiltersList, selectSelectedFilters, toggleFilter } from '../../../services/slices/filter/filterSlice'
 import styles from './styles.module.scss'
 
-// export function CheckboxInput({
-//   children,
-//   options,
-//   active = false,
-//   opened = false,
-//   onChange,
-//   onOpen,
-// }: CheckboxInputProps) {
-//   const [states, setStates] = useState<Record<string, boolean>>(() =>
-//     options?.reduce((acc, option) => {
-//       acc[option] = false
-//       return acc
-//     }, {} as Record<string, boolean>),
-//   )
-
-//   return (
-//     <>
-//       <div className={styles['checkbox-container']}>
-//         <label className={styles.label}>
-//           <input
-//             type="checkbox"
-//             checked={active}
-//             onChange={onChange}
-//             className={styles.input}
-//             disabled={!onChange}
-//           />
-//           <svg className={styles['checkbox-icon']}>
-//             <use href={`#icon-${active ? 'checkbox-remove' : 'checkbox-empty'}`} />
-//           </svg>
-//           <span className={styles.text}>{children}</span>
-//         </label>
-//         <button onClick={() => onOpen()} className={styles['arrow-button']}>
-//           <svg className={`${styles.arrow} ${!opened && styles['arrow-closed']}`}>
-//             <use href="#icon-arrow" />
-//           </svg>
-//         </button>
-//       </div>
-
-//       {options && opened && (
-//         <ul className={styles.options}>
-//           {options.map(option => (
-//             <li key={option} className={styles.option}>
-//               <label className={styles.label}>
-//                 <div className={styles['checkbox-container']}>
-//                   <input
-//                     type="checkbox"
-//                     checked={states[option]}
-//                     onChange={() => setStates(prev => ({ ...prev, [option]: !prev[option] }))}
-//                     className={styles.input}
-//                     disabled={!onChange}
-//                   />
-//                   <svg className={styles['checkbox-icon']}>
-//                     <use href={`#icon-${states[option] ? 'checkbox-done' : 'checkbox-empty'}`} />
-//                   </svg>
-//                   <span className={styles.text}>{option}</span>
-//                 </div>
-//               </label>
-//             </li>
-//           ))}
-//         </ul>
-//       )}
-//     </>
-//   )
-// }
-
-// function Checkbox() {
-//   return (
-//     <>
-//       <div className={styles['checkbox-container']}>
-//         <label className={styles.label}>
-//           <input type="checkbox" checked={active} onChange={onChange} className={styles.input} disabled={!onChange} />
-//           <svg className={styles['checkbox-icon']}>
-//             <use href={`#icon-${active ? 'checkbox-remove' : 'checkbox-empty'}`} />
-//           </svg>
-//           <span className={styles.text}>{children}</span>
-//         </label>
-//         <button onClick={() => onOpen()} className={styles['arrow-button']}>
-//           <svg className={`${styles.arrow} ${!opened && styles['arrow-closed']}`}>
-//             <use href="#icon-arrow" />
-//           </svg>
-//         </button>
-//       </div>
-//     </>
-//   )
-// }
-
 export function FiltersPanel() {
   const dispatch = useDispatch()
   const filters = useSelector(selectFiltersList)
@@ -149,34 +63,6 @@ export function FiltersPanel() {
               </ul>
             )}
           </>
-
-          // <div key={parent.id}>
-          //   <label>
-          //     <input
-          //       type="checkbox"
-          //       checked={selectedIds.includes(parent.id)}
-          //       onChange={() => dispatch(toggleFilter(parent.id))}
-          //     />
-          //     {parent.name}
-          //   </label>
-          //   {parent.children.length > 0 && (
-          //     <div style={{ paddingLeft: 20 }}>
-          //       {parent.children.map((childId) => {
-          //         const child = filters.find(f => f.id === childId)
-          //         return (
-          //           <label key={child.id}>
-          //             <input
-          //               type="checkbox"
-          //               checked={child.checked}
-          //               onChange={() => dispatch(toggleFilter(child.id))}
-          //             />
-          //             {child.name}
-          //           </label>
-          //         )
-          //       })}
-          //     </div>
-          //   )}
-          // </div>
         ))}
       </ul>
     </div>
