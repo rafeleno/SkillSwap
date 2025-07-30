0b15e14e368a6d6b31cd2d3ae026aa9fd950ea49
Ivan <linnorm@ya.ru>
2025-07-30 11:44:32 +0300

    refactor: use Tag-component

--- src/widgets/UserCard/UserCard.tsx
+++ src/widgets/UserCard/UserCard.tsx
@@ -1,6 +1,8 @@
+import type { TagVariant } from '../../shared/ui/Tag/Tag.types'
 import type { UserCardProps } from './UserCard.types'
 import { LikeButton } from '@uiComponents/LikeButton'
 import { MainButton } from '@uiComponents/MainButton'
+import { Tag } from '@uiComponents/Tag'
 import React from 'react'
 import styles from './styles.module.scss'
 
@@ -9,21 +11,10 @@ export const UserCard: React.FC<UserCardProps> = ({ User, onClick, onLike }) => 
 
   const handleLike = () => onLike(id)
 
-  const skillClasses = {
-    business_and_career: styles.businessAndCareer,
-    health_and_lifestyle: styles.healthAndLifestyle,
-    creativity_and_art: styles.creativityAndArt,
-    education_and_development: styles.educationAndDevelopment,
-    foreign_languages: styles.foreignLanguages,
-    home_and_comfort: styles.homeAndComfort,
-  }
-
   return (
     <article
       className={styles.userCard}
       aria-label={`Карточка пользователя ${name}`}
-      role="region"
-      tabIndex={0}
     >
       <header className={styles.header}>
         <img
@@ -50,31 +41,17 @@ export const UserCard: React.FC<UserCardProps> = ({ User, onClick, onLike }) => 
       <div className={styles.sections}>
         <section className={styles.section} aria-labelledby={`teach-${id}`}>
           <h4 className={styles.label} id={`teach-${id}`}>Может научить:</h4>
-          <span
-            className={`${styles.teachTag} ${skillClasses[skillCanTeach.skillId] || styles.defaultSkill}`}
-            role="text"
-          >
-            {skillCanTeach.name}
-          </span>
+          <Tag variant={skillCanTeach.subcategoryId as TagVariant} />
         </section>
 
         <section className={styles.section} aria-labelledby={`want-${id}`}>
           <h4 className={styles.label} id={`want-${id}`}>Хочет научиться:</h4>
           <div className={styles.wantTags}>
             {subcategoriesWantToLearn.slice(0, 2).map(skill => (
-              <span
-                key={skill.skillId + skill.name}
-                className={`${styles.wantTag} ${skillClasses[skill.skillId] || styles.defaultSkill}`}
-                role="text"
-              >
-                {skill.name}
-              </span>
+              <Tag key={skill.skillId + skill.name} variant={skill.subcategoryId as TagVariant} />
             ))}
             {subcategoriesWantToLearn.length > 2 && (
-              <span className={styles.moreTag}>
-                +
-                {subcategoriesWantToLearn.length - 2}
-              </span>
+              <Tag variant="more" count={subcategoriesWantToLearn.length - 2} />
             )}
           </div>
         </section>
