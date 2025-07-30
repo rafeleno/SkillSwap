c4700b5338a958de8dec65376b062e8b3e7549fe
Allen11137 <a.zukkel@>
2025-07-30 17:39:39 +0300

    refactoring/DropDown

--- src/shared/ui/Dropdown/Dropdown.types.ts
+++ src/shared/ui/Dropdown/Dropdown.types.ts
@@ -1,21 +1,24 @@
-import type React from 'react'
+import React from 'react';
+
+export type DropdownVersion = 'default' | 'no-border';
+export type DropdownPosition = 'relative' | 'absolute';
 
 export interface DropdownOption {
-  value: string
-  label: string
-  content?: React.ReactNode
+  value: string;
+  label: string;
+  content?: React.ReactNode;
 }
 
 export interface DropdownProps {
-  id: string
-  options: DropdownOption[]
-  selectedValue?: string
-  onSelect?: (value: string) => void
-  label?: string
-  placeholder?: string
-  isExpanded: boolean
-  onExpandToggle: () => void
-  displayText?: string
-  version?: 'default' | 'no-border'
-  position?: 'absolute' | 'relative'
+  id: string;
+  options: DropdownOption[];
+  selectedValue?: string;
+  onSelect?: (value: string) => void;
+  label?: string;
+  placeholder?: string;
+  isExpanded: boolean;
+  onExpandToggle: () => void;
+  displayText?: string;
+  version?: DropdownVersion;
+  position?: DropdownPosition;
 }
