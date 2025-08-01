2b5cda2a95da2047612aa84ddcea533467ab99d4
Nastya Orlova <web.orlova.dev@gmail.com>
2025-08-01 18:23:27 +0300

    feat: Реализация RegisterModal

--- src/widgets/RegisterModal/RegisterModal.tsx
+++ src/widgets/RegisterModal/RegisterModal.tsx
@@ -1,12 +1,34 @@
 import type { RegisterModalProps } from './RegisterModal.types'
 import { MainButton } from '@uiComponents/MainButton'
 import { MainLogo } from '@uiComponents/MainLogo'
-import React from 'react'
+import { RegisterModalContent } from '@uiComponents/RegisterModalContent'
+import React, { useState } from 'react'
+import { selectAllCategories } from '../../services/slices/skill/skillSlice'
+import { selectCurrentUser, updateUserField } from '../../services/slices/users/userSlice'
+import { useDispatch, useSelector } from '../../services/store'
 import styles from './styles.module.scss'
 
-export const RegisterModal: React.FC<RegisterModalProps> = ({ onClose }) => {
-  const totalSteps = 3
-  const currentStep = 1
+export const RegisterModal: React.FC<RegisterModalProps> = ({ onClose, totalSteps = 3 }) => {
+  const [currentStep, setCurrentStep] = useState(1)
+  const [password, setPassword] = useState('') // Только для pass
+  const user = useSelector(selectCurrentUser)
+  const categories = useSelector(selectAllCategories)
+  const dispatch = useDispatch()
+
+  const updateField = (field: keyof typeof user, value: any) => {
+    dispatch(updateUserField({ field, value }))
+  }
+
+  const nextStep = () => setCurrentStep(prev => prev + 1)
+  const prevStep = () => setCurrentStep(prev => prev - 1)
+
+  const stepTypeMap = {
+    1: 'stepOne',
+    2: 'stepTwo',
+    3: 'stepThree',
+  } as const
+
+  const getStepType = () => stepTypeMap[currentStep as keyof typeof stepTypeMap] || 'stepOne'
 
   return (
     <div>
@@ -23,7 +45,13 @@ export const RegisterModal: React.FC<RegisterModalProps> = ({ onClose }) => {
 
       <div className={styles.progress}>
         <h2 className={styles.progressText}>
-          Шаг 1 из 3
+          Шаг
+          {' '}
+          {currentStep}
+          {' '}
+          из
+          {' '}
+          {totalSteps}
         </h2>
         <div className={styles.progressBar}>
           {Array.from({ length: totalSteps }, (_, index) => (
@@ -35,9 +63,20 @@ export const RegisterModal: React.FC<RegisterModalProps> = ({ onClose }) => {
         </div>
       </div>
 
-      {/* Контент текущего шага, пусть пока будет так. Нам всеравно пофиг, что будет за контент */}
       <main className={styles.content}>
-        <div>Контент шага 1 - Email и пароль</div>
+        <RegisterModalContent
+          type={getStepType()}
+          onSubmit={nextStep}
+          onPrev={prevStep}
+
+          // Данные из Redux
+          user={user}
+          categories={categories}
+          onUpdateUser={updateField}
+
+          // Локальные данные
+          passwordState={[password, setPassword]}
+        />
       </main>
     </div>
   )
--- src/widgets/RegisterModal/RegisterModal.types.ts
+++ src/widgets/RegisterModal/RegisterModal.types.ts
@@ -1,27 +1,4 @@
 export interface RegisterModalProps {
   onClose: () => void
+  totalSteps?: number
 }
-
-// Типы для всех данных регистрации. Может вынести в глобальный types?
-export interface RegistrationData {
-  // Шаг 1
-  email: string
-  password: string
-
-  // Шаг 2
-  name: string
-  birthDate: string
-  gender: string
-  city: string
-  learningCategory: string
-  learningSubcategory: string
-
-  // Шаг 3
-  skillTitle: string
-  skillCategory: string
-  skillSubcategory: string
-  skillDescription: string
-  skillImages: File[]
-}
-
-export type StepType = 1 | 2 | 3
