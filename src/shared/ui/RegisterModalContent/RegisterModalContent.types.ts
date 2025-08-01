78b601aa48680ab465ac69c615617ac5f89749f8
Nastya Orlova <web.orlova.dev@gmail.com>
2025-08-01 18:22:44 +0300

    fix: Подключила RegisterModalContent к Redux store, добавила заглушки для других шагов

--- src/shared/ui/RegisterModalContent/RegisterModalContent.types.ts
+++ src/shared/ui/RegisterModalContent/RegisterModalContent.types.ts
@@ -1,10 +1,17 @@
-import type { InputType } from '@uiComponents/PrimaryTextInput/Input.types'
 import type { useState } from 'react'
+import type { Category } from 'services/slices/skill/skillSlice'
+import type { User } from 'services/slices/users/userSlice'
+import type { InputType } from '../../ui/PrimaryTextInput/Input.types'
 
 export interface RegisterModalContentProps {
   type: 'stepOne' | 'stepTwo' | 'stepThree'
   onSubmit: () => void
-  emailState: ReturnType<typeof useState<string>>
+  onPrev?: () => void
+  user: User | null
+  categories: Category[]
+  onUpdateUser: (field: keyof User, value: any) => void
+
+  // Локальные данные (только пароль)
   passwordState: ReturnType<typeof useState<string>>
 }
 
