9dc0a30ea78bf29f872de39b87b3ebf538382136
Nastya Orlova <web.orlova.dev@gmail.com>
2025-08-01 18:16:30 +0300

    fix: Убрала link у MainLogo и добавила тип TInitialInputs

--- src/shared/ui/MainLogo/MainLogo.tsx
+++ src/shared/ui/MainLogo/MainLogo.tsx
@@ -1,15 +1,15 @@
 import type { MainLogoProps } from './MainLogo.types'
 import React from 'react'
-import { Link } from 'react-router-dom'
 import styles from './styles.module.scss'
 
 export const MainLogo: React.FC<MainLogoProps> = () => {
   return (
-    <Link className={styles.mainLogo} to="/">
+    // Убрала link
+    <div className={styles.mainLogo}>
       <div className={styles.iconWrapper}>
         <span className={styles.icon}></span>
       </div>
       <h2 className={styles.text}>SkillSwap</h2>
-    </Link>
+    </div>
   )
 }
