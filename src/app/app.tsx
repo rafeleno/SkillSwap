edf38dec00a47ddb13151f0583f1a73300caeae3
Victor Baskakov @rafeleno/viktorbaskakov356@gmail.com <viktorbaskakov356@gmail.com>
2025-08-02 16:44:24 +0300

    fix: clear app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,17 +1,16 @@
 import { MainPage } from '@pageComponents/MainPage'
 import { Header } from '@widgetComponents/Header'
 import React from 'react'
-import styles from './styles.module.scss'
 
 export function App() {
   return (
-    <div className={styles.container}>
+    <>
       {/* передать из слайса */}
       <Header user={null}></Header>
       <main className="skill-swap-main">
         <MainPage></MainPage>
       </main>
       <footer></footer>
-    </div>
+    </>
   )
 }
