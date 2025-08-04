0f9309c5dec4f720c068fd46983ec4ea2a178904
Victor Baskakov @rafeleno/viktorbaskakov356@gmail.com <viktorbaskakov356@gmail.com>
2025-08-04 23:47:40 +0300

    feat: custom layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,23 +1,15 @@
-import { Footer } from '@widgetComponents/Footer'
-import { Header } from '@widgetComponents/Header'
 import React from 'react'
+import users from '../../public/db/users.json'
+import { initFilterSettings } from '../services/slices/filter/filterSlice'
 import { fetchSkills } from '../services/slices/skill/thunks'
 import store from '../services/store'
 import { AppRouter } from './router'
-import { initFilterSettings } from '../services/slices/filter/filterSlice'
-import users from '../../public/db/users.json'
-
 
 export function App() {
-
   store.dispatch(fetchSkills())
-  store.dispatch(initFilterSettings(users));
+  store.dispatch(initFilterSettings(users))
 
   return (
-    <>
-      <Header user={null} />
-      <AppRouter />
-      <Footer />
-    </>
+    <AppRouter />
   )
 }
--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,61 +1,96 @@
+import { Footer } from '@widgetComponents/Footer'
+import { Header } from '@widgetComponents/Header'
 import React, { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { Pages } from '../pages'
 
+import { Outlet, Route, Routes } from 'react-router-dom'
+import { Pages } from '../pages'
 import { ProtectedRoute } from '../shared/lib/components/ProtectedRoute'
+
+export function DefaultLayout() {
+  return (
+    <>
+      {/* TODO: вытяннуть из слайса */}
+      <Header user={null} />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+// routes/CustomLayout.tsx
+export function CustomLayout() {
+  return (
+    <>
+      <Outlet />
+    </>
+  )
+}
 
 export function AppRouter() {
   return (
     <Suspense fallback={<div>Загрузка...</div>}>
       <Routes>
-        <Route path="/" element={<Pages.Catalog />} />
-        <Route path="/skill/:id" element={<Pages.Skill />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<Pages.Catalog />} />
+        </Route>
+        <Route element={<DefaultLayout />}>
+          <Route path="/skill/:id" element={<Pages.Skill />} />
+        </Route>
 
         {/* Только для неавторизованных */}
-        <Route
-          path="/login"
-          element={(
-            <ProtectedRoute onlyUnAuth>
-              <Pages.Login />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/register"
-          element={(
-            <ProtectedRoute onlyUnAuth>
-              <Pages.Register />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
+        <Route element={<CustomLayout />}>
+          <Route
+            path="/login"
+            element={(
+              <ProtectedRoute onlyUnAuth>
+                <Pages.Login />
+              </ProtectedRoute>
+            )}
+          />
+        </Route>
+        <Route element={<CustomLayout />}>
+          <Route
+            path="/register"
+            element={(
+              <ProtectedRoute onlyUnAuth>
+                <Pages.Register />
+              </ProtectedRoute>
+            )}
+          />
+        </Route>
+        {/* <Route
           path="/create"
           element={(
             <ProtectedRoute onlyUnAuth>
               <Pages.Onboarding />
             </ProtectedRoute>
           )}
-        />
+        /> */}
 
         {/* Только для авторизованных */}
-        <Route
-          path="/profile"
-          element={(
-            <ProtectedRoute>
-              <Pages.Profile />
-            </ProtectedRoute>
-          )}
-        />
-        <Route
-          path="/favorites"
-          element={(
-            <ProtectedRoute>
-              <Pages.Favorites />
-            </ProtectedRoute>
-          )}
-        />
-
-        <Route path="*" element={<Pages.NotFound404 />} />
+        <Route element={<DefaultLayout />}>
+          <Route
+            path="/profile"
+            element={(
+              <ProtectedRoute>
+                <Pages.Profile />
+              </ProtectedRoute>
+            )}
+          />
+        </Route>
+        <Route element={<DefaultLayout />}>
+          <Route
+            path="/favorites"
+            element={(
+              <ProtectedRoute>
+                <Pages.Favorites />
+              </ProtectedRoute>
+            )}
+          />
+        </Route>
+        <Route element={<DefaultLayout />}>
+          <Route path="*" element={<Pages.NotFound404 />} />
+        </Route>
       </Routes>
     </Suspense>
   )
