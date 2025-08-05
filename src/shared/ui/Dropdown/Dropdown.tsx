575d115650c31fbccf0b0264b0eea053b48fbc77
Katherine Vasilevskaya <katevasilevskaya0@gmail.com>
2025-08-05 23:25:35 +0300

    fix: large text in dropdown

--- src/shared/ui/Dropdown/Dropdown.tsx
+++ src/shared/ui/Dropdown/Dropdown.tsx
@@ -82,13 +82,24 @@ export const Dropdown: React.FC<DropdownProps> = ({
               />
             )
           : (
-              isCheckbox && Array.isArray(selectedOption)
-                ? selectedOption.length > 0
-                  ? selectedOption.map(o => o.value).join(', ')
-                  : label
-                : selectedOption
-                  ? (selectedOption as IOption).value
-                  : label
+              <>
+                {
+                  isCheckbox && Array.isArray(selectedOption)
+                    ? (
+                        <span className={styles['selected-value']}>
+                          {selectedOption.length > 0
+                            ? selectedOption.map(o => o.value).join(', ')
+                            : label}
+                        </span>
+                      )
+                    : (
+                        (selectedOption && 'value' in selectedOption)
+                          ? selectedOption.value
+                          : label
+                      )
+                }
+
+              </>
             )}
 
         {searchable && searchTerm
@@ -123,10 +134,7 @@ export const Dropdown: React.FC<DropdownProps> = ({
                   <li
                     key={option.id}
                     onClick={() => {
-                      if (isCheckbox) {
-                        handleCheckboxToggle(option)
-                      }
-                      else {
+                      if (!isCheckbox) {
                         onChange(option)
                         setIsOpen(false)
                         setSearchTerm(option.value)
