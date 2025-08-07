96bfdb695cf384dc5d375f89b6709fdf35215a50
Guzel <guzel.zhagfarovna@gmail.com>
2025-08-07 21:35:38 +0300

    fix: implementing like functionality in favorites-component, connecting to userSlice (#191)

--- src/widgets/FavoritesComponent/FavoritesComponent.tsx
+++ src/widgets/FavoritesComponent/FavoritesComponent.tsx
@@ -1,19 +1,30 @@
 import type { FavoritesProps } from './FavoritesComponent.types'
 import { UserCardList } from '@widgetComponents/UserCardList'
 import React from 'react'
-
-import users from '../../../public/db/users.json'
+import { useNavigate } from 'react-router-dom'
+import { selectCurrentUser, selectUsers, toggleFavourites } from '../../services/slices/user/userSlice'
+import { useDispatch, useSelector } from '../../services/store'
 import styles from './styles.module.scss'
 
 export const Favorites: React.FC<FavoritesProps> = () => {
-  const favoriteUsers = users
-    .filter(user => user.likes >= 50)
-    .sort((a, b) => b.likes - a.likes)
-    .slice(0, 3)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const currentUser = useSelector(selectCurrentUser)
+  const allUsers = useSelector(selectUsers) || []
+  const favoriteUsers = allUsers.filter(user => currentUser?.favourites?.includes(user.id)) || []
+
+  const handleCard = (userId: string) => {
+    navigate(`/skills/${userId}`)
+  }
+
+  const handleLike = (userId: string) => {
+    dispatch(toggleFavourites(userId))
+  }
 
   return (
     <div className={styles.favorites}>
-      <UserCardList type="regular" title="Избранное" buttonIconId="chevron-right" users={favoriteUsers} />
+      <UserCardList onCardClick={handleCard} onLike={handleLike} type="regular" title="Избранное" buttonIconId="chevron-right" users={favoriteUsers} />
     </div>
   )
 }
